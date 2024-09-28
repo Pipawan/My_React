bae16b6581966517817dd02e72d19061c4b03bc0
I_am_pie <enepali126@gmail.com>
2024-09-28 17:29:20 -0400

    Navbar css

--- MyPortfolio/src/components/Navbar.jsx
+++ MyPortfolio/src/components/Navbar.jsx
@@ -1,13 +1,29 @@
+import  { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS for the Navbar
 
-const Navbar = () => (
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/about">About Me</Link>
-    <Link to="/projects">Projects</Link>
-    <Link to="/services">Services</Link>
-    <Link to="/contact">contact Me</Link>
-  </nav>
-);
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">MyPortfolio</div>
+      <div className="nav-toggle" onClick={toggleMenu}>
+        &#9776; {/* Hamburger icon */}
+      </div>
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About Me</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact Me</Link></li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
